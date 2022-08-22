c2ffda557e2ad14ecd758fc951274d81959cff0c
Victor Ayala <victor.ayala2a@gmail.com>
2022-08-22 12:12:49 -0400

    Update sso_config

--- sgp/settings.py
+++ sgp/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+
+    # Crispy Form
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
