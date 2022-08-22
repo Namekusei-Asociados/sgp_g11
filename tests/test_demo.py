733794715cbe7df9f32ce19fecd70fcca5f782c3
notarioale <alealenotario@gmail.com>
2022-08-22 13:20:25 -0400

    test removed

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -4,9 +4,3 @@ import pytest
 # Test the Pytest library
 def test_working():
     pass
-
-
-# Test the admin view
-def test_admin_view(admin_client):
-    response = admin_client.get('/admin/')
-    assert response.status_code == 200
