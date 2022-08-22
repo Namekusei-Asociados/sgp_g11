6646f0d4286c464375c8e38d4c503304da520e0d
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-08-22 11:04:57 -0400

    Se corrigen formatos de los documentos

--- sgp/wsgi.py
+++ sgp/wsgi.py
@@ -4,6 +4,7 @@ WSGI config for sgp project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
     - El wsgi a ser utilizado por el grupo 11 es Gunicorn"""
 
