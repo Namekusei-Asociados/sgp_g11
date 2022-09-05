d1ace53969909c71e1617994234deba45e0eaa9e
cristhian-ortellado <breideworld@gmail.com>
2022-09-05 01:05:53 -0400

    test again

--- sgp/wsgi.py
+++ sgp/wsgi.py
@@ -10,11 +10,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 import dotenv
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sgp.settings')
-
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
