5b7a677a6d345ec76c4541646e91caefefee992e
cristhian-ortellado <breideworld@gmail.com>
2022-09-07 18:03:25 -0400

    url names added; new url to store projects added

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.create, name='projects.create'),
+    path('store', views.store, name='projects.store')
 ]
