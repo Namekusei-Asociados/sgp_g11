ac7c8403100ab6ec8d3dc611c66c0f51abeb0542
cristhian-ortellado <breideworld@gmail.com>
2022-09-08 02:06:24 -0400

    create, edit and list for projects

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,15 @@ from .models import Project
 from accounts.models import User
 from django.contrib import messages
 from django.urls import reverse
+
+
 # Create your views here.
+def index(request):
+    # get all projects
+    projects = Project.objects.all()
+    return render(request, 'index.html', {"projects": projects})
+
+
 def create(request):
     """
     Retorna un formulario de creacion para proyectos
@@ -20,7 +28,7 @@ def store(request):
     :param request:
     :return:
     """
-
+    # getting attributes
     name = request.POST['name']
     description = request.POST['description']
     user_id = request.POST['user']
@@ -40,3 +48,52 @@ def store(request):
     # redirect back with success message
     messages.success(request, 'EL proyecto fue creado con exito')
     return redirect(reverse('projects.create'), request)
+
+
+def edit(request, id):
+    """
+    Retorna la vista de edicion del projecto actual
+    :param request:
+    :param id: campo del modelo Project
+    :return: formulario de edicion de proyecto
+    """
+    # get project
+    project = Project.objects.get(id=id)
+    users = User.objects.all()
+    members = project.members.all()
+    return render(request, 'edit.html', {'project': project, 'users': users, 'members': members})
+
+
+def update(request):
+    """
+    Actualiza un recurso del modelo Project
+    :param request: posee los campos a modificar
+    :param id: campo del modelo Project
+    :return: formulario de edicion de proyecto
+    """
+    # get fields from edit form
+    name = request.POST['name']
+    description = request.POST['description']
+    users = request.POST.getlist('users[]')
+    project_id = request.POST['project_id']
+
+    # get project and update
+    project = Project.objects.get(id=project_id)
+
+    # detach members
+    members = project.members.values_list('id', flat=True)
+    print(users)
+    for member in members:
+        if str(member) not in users:
+            project.members.remove(str(member))
+
+    # attach members
+    project.members.add(*users)
+
+    # update fields
+    project.name = name
+    project.description = description
+    project.save()
+
+    messages.success(request, 'EL proyecto fue actualizado con exito')
+    return redirect(reverse('projects.edit', kwargs={'id': project.id}), request)
