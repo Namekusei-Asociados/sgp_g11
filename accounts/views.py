89aac02da844e97b7716e616923169fa274c7365
VikMannu <victor.ayala2a@gmail.com>
2022-09-07 22:47:32 -0400

    Add functions validate user and email

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from accounts.forms import SignupForm, UserForm
+from accounts.forms import SignupForm, UserForm, username_exists, email_exists
+from accounts.models import User
 
 
 @login_required()
@@ -25,13 +27,31 @@ def validate_user(request):
         # create a form instance and populate it with data from the request:
         form = UserForm(request.POST)
         # check whether it's valid:
+        print(form)
         if form.is_valid():
-            return render(request, 'thanks.html', {'info': request.POST['username']})
+            # process the data in form.cleaned_data as required
+            username = request.POST['username']
+            email = request.POST['email']
+            pass1 = request.POST['password1']
+            pass2 = request.POST['password2']
+            if username_exists(username):
+                return render(request, 'error.html', {'info': username})
+
+            if email_exists(email):
+                return render(request, 'error.html', {'info': email})
+
+            if pass1 != pass2:
+                return render(request, 'error.html', {'info': f'{pass1} | {pass2}'})
+
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
         else:
             print('---------USERNAME ERROR--------------')
             return render(request, 'error.html', {'info': 'else isn\'n valid'})
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        print('---------USERNAME ERROR--------------')
-        return render(request, 'error.html', {'info': 'else reques method'})
+        form = UserForm()
+
+    return render(request, 'error.html', {'form': form})
+
