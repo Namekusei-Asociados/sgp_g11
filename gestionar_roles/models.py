9f9fbb33d9b3c777a77ee29fdbe24f40380cd989
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-09-08 01:48:29 -0400

    se corrigen funciones de crear roles, asignar y desasignar permisos

--- gestionar_roles/models.py
+++ gestionar_roles/models.py
@@ -3,79 +3,84 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from guardian.shortcuts import assign_perm, remove_perm
 
+class RoleManager(models.Manager):
+    # funcion para crear roles permisos por modelo
+    def create_role(self, name, description, type_role, permissions_list):
+        group_role = Group.objects.create(name=name)
+        role = Role.objects.create(
+            role_name=name,
+            description=description,
+            type_role=type_role,
+            role_group=group_role
+        )
+        role.save()
+        self.attach_permissions(role.id, permissions_list)
+        print('rol creado exitosamente')
 
-# Create your models here.
-class Role(models.Model):
+    # funcion encargada de asignar permisos a los grupos de roles
+    def attach_permissions(self, id_role, permissions_list):
+        role = Role.objects.get(id=id_role)
+        print(role)
+        nombre=role.role_name
+        group_role = Group.objects.get(name=nombre)
+        for permiso_temp in permissions_list:
+            assign_perm(permiso_temp, group_role)
 
-    # campos de los roles en la base de datos
-    id = models.IntegerField
-    role_name = models.CharField(max_length=40, null=False)
-    description = models.CharField(max_length=250, null=True)
-    # asociamos el rol a la tabla de grupos
-    role_group = models.ForeignKey(Group, on_delete=models.CASCADE, null=False)
-    type = models.IntegerField    #1- Sistema, 2- Proyecto
-    def __str__(self):
-        return self.role_name
 
-    #funcion para crear roles permisos por modelo
-    def create_role(self, name, description, type, *permissions):
-        self.role_name=name; self.description=description; self.type=type;
-        self.save();
-        group_role, created = Group.objects.get_or_create(name=f'{name}')
-        if created:
-            self.attach_permissions(group_role, permissions)
+    # funcion encargada de asignar permisos a los grupos de roles
+    def dettach_permissions(self, id_role, permissions_list):
+        role = Role.objects.get(id=id_role)
+        group_role = Group.objects.get(name=role.role_name)
+        for permiso_temp in permissions_list:
+            remove_perm(permiso_temp, group_role)
 
-    #funcion encargada de asignar permisos a los grupos de roles
-    def attach_permissions(self, group_role, *permissions):
-        for permiso_temp in permissions:
-            if Permission.objects.filter(name=permiso_temp).exists():
-                assign_perm(permiso_temp, group_role)
-            else:
-                print("el permiso no existe")
+    #verifica la existencia de un permiso en la base de datos
+    def exist_perm(self, perm):
+        if Permission.objects.filter(codename=perm).exists():
+            return True
+        else:
+            return False
 
-    #funcion encargada de asignar permisos a los grupos de roles
-    def dettach_permissions(self, group_role, *permissions):
-        for permiso_temp in permissions:
-            if Permission.objects.filter(name=permiso_temp).exists():
-                remove_perm(permiso_temp, group_role)
-            else:
-                print("el permiso no existe")
-
-    #funcion para asignar rol a usuario
+    # funcion para asignar rol a usuario
     def assign_role_to_user(self, id_role, user):
         try:
             rol = Role.objects.filter(id=id_role)
             grupo = Group.objects.get(id=self.role_group.id)
             grupo.user_set.add(user)
         except Role.DoesNotExist as error:
-            print("El rol no existe"+id_role)
-    #funcion para asignar rol a usuario
+            print("El rol no existe" + id_role)
+
+
+    # funcion para asignar rol a usuarioFo
     def unassign_role_to_user(self, id_role, user):
         try:
             role = Role.objects.filter(id=id_role)
             group_role = Group.objects.get(id=role.role_group.id)
             group_role.user_set.remove(user)
         except Role.DoesNotExist as error:
-            print("El rol no existe"+id_role)
+            print("El rol no existe" + id_role)
 
-    #funcion para eliminar un rol
+
+    # funcion para eliminar un rol
     def delete_role(self, id_role):
         try:
-            role= Role.objects.filter(id=id_role)
-            group_role=Group.objects.filter(id=role.role_group.id)
+            role = Role.objects.filter(id=id_role)
+            group_role = Group.objects.filter(id=role.role_group.id)
         except Role.DoesNotExist as error:
-            print("El rol no existe"+id_role)
+            print("El rol no existe" + id_role)
+
 
     # funcion para crear roles de permisos por objeto
     def create_role_per_object(self, name, description, type, *permissions, object):
-        #rellenamos los campos
-        self.role_name = name; self.description = description; self.type = type;
+        # rellenamos los campos
+        self.role_name = name;
+        self.description = description;
+        self.type = type;
         group_role, created = Group.objects.get_or_create(name=f'{id}+{object.id}')
         if created:
             self.attach_permissions(group_role, permissions, object)
 
-        # funcion encargada de asignar permisos a los grupos de roles
-
+    # funcion encargada de asignar permisos a los grupos de roles
     def attach_permissions_per_object(self, group_role, *permissions, object):
         for permiso_temp in permissions:
             if Permission.objects.filter(name=permiso_temp).exists():
@@ -83,7 +88,37 @@ class Role(models.Model):
             else:
                 print("el permiso no existe")
 
-    #funcion para listar los roles existentes
-    def list_roles(self):
-        return self.objects.all()
 
+
+    # funcion para listar los roles existentes
+    def list_roles(self):
+        return Role.objects.all()
+
+    # funcion para listar permisos del rol
+    def list_roles_perms(self, id_role):
+        try:
+            role = Role.objects.get(id=id_role)
+            group_role = Group.objects.get(namei=role.role_name)
+            return group_role.permissions.all()
+        except Role.DoesNotExist as error:
+            print("El rol no existe" + id_role)
+
+# Create your models here.
+class Role(models.Model):
+
+    # campos de los roles en la base de datos
+    role_name = models.CharField(max_length=40, null=True)
+    description = models.CharField(max_length=250, null=True)
+    # asociamos el rol a la tabla de grupos
+    type_role = models.IntegerField()    #1- Sistema, 2- Proyecto
+    role_group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+
+    objects=RoleManager()
+
+    def __str__(self):
+        return self.role_name
+
+    class Meta:
+        permissions = (
+            ('list_role', 'Can list role'),
+        )
