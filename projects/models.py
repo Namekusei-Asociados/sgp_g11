9ff3f1c37ab2805d209f5f0d85d1a80536989e6a
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-09-24 10:58:27 -0400

    impresion de roles de sistema en la lista

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.db import models
 from accounts.models import User
-
-
+from gestionar_roles.models import Permissions
+class RoleProjectManager(models.Manager):
+    pass
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=50)
@@ -15,4 +16,19 @@ class Project(models.Model):
     # def __str__(self) -> str:
     #     text = "{0}"
     #     return text.format(self.name)
+
+class RoleProject(models.Model):
+    """
+    Roles de sistema
+    """
+    # campos de los roles en la base de datos
+    role_name = models.CharField(max_length=40, null=True)
+    description = models.CharField(max_length=250, null=True)
+    # asociamos los roles a permisos
+    perms = models.ManyToManyField(Permissions)
+
+    objects = RoleProjectManager()
+
+    def __str__(self):
+        return f'{self.role_name}'
 
