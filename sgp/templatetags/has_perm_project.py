b0725ea6eadbc24c6dfa58252c81e5d5251f8c64
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-09-28 18:21:41 -0400

    has perm de proyectos

---
+++ sgp/templatetags/has_perm_project.py
@@ -0,0 +1,12 @@
+from django import template
+from projects.models import RoleProject
+
+register = template.Library()
+
+
+@register.simple_tag
+def has_perm_project(user, id_project, perms):
+    """Decorador para verificar si un usuario posee el permiso de proyecto solicitado"""
+    return RoleProject.objects.has_permissions(user.id, id_project, perms)
+
+
