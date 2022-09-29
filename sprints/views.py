6e55ccd3bcd3e976a4e974f25cb7cd8d026453e9
Victor Ayala <victor.ayala2a@gmail.com>
2022-09-28 20:17:29 -0400

    Last version

--- sprints/views.py
+++ sprints/views.py
@@ -1,6 +1,11 @@
+import datetime
+
 from django.shortcuts import render
+from dateutil.rrule import DAILY, rrule, MO, TU, WE, TH, FR
+import pandas as pd
+
+from user_story.models import UserStory
 from .models import Sprint
-from accounts.models import User
 
 
 # Create your views here.
@@ -15,18 +20,36 @@ def index(request, id_project):
 
     :return: documento HTML
     """
-    sprints = Sprint.objects.all()
-    context = {'sprints': sprints, 'id_project': id_project}
+    sprints = Sprint.objects.filter(project_id=id_project)
+    context = {
+        'sprints': sprints,
+        'id_project': id_project
+    }
+
     return render(request, 'sprint/index.html', context)
 
 
 def create_sprint(request, id_project):
-    user_stories = User.objects.all()
-    user_stories = filter(lambda x: x.role_sys != 'visitor' and not x.is_staff, user_stories)
-    context = {"user_stories": user_stories, 'id_project': id_project}
+    user_stories = UserStory.objects.filter(project_id=id_project)
+    context = {
+        "user_stories": user_stories,
+        'id_project': id_project
+    }
+
     return render(request, 'sprint/create_sprint.html', context)
 
 
-def validate_create_sprint(request):
-    sprint_name = request.POST("")
+def validate_create_sprint(request, id_project):
+    sprint_name = request.POST['sprint_name']
+    capacity = request.POST['capacity']
+    start_at = request.POST['start_at']
+    end_at = request.POST['end_at']
+    duration = pd.bdate_range(start=start_at, end=end_at).size
+
+    user_stories = request.POST.getlist('user_stories[]')
+
     return None
+
+
+def daterange(start_date, end_date):
+    return rrule(DAILY, dtstart=start_date, until=end_date, byweekday=(MO, TU, WE, TH, FR))
