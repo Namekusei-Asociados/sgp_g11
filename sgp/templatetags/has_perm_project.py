7b6c96449183ca617ce24711270b16effcab64f5
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-10-14 18:33:54 -0300

    icons of projects

--- sgp/templatetags/has_perm_project.py
+++ sgp/templatetags/has_perm_project.py
@@ -14,3 +14,8 @@ def has_perm_project(user, id_project, perms):
 @register.simple_tag
 def is_scrum_master(member):
     return member.roles.filter(role_name=UProjectDefaultRoles.SCRUM_MASTER).exists()
+
+
+@register.simple_tag
+def is_member(user, id_project):
+    return ProjectMember.objects.filter(user_id=user.id, project_id=id_project).exists()
