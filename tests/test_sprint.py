08db767c928d600272c435313552300c67589e78
notarioale <alealenotario@gmail.com>
2022-10-17 03:49:14 -0300

    updated all my tests

--- tests/test_sprint.py
+++ tests/test_sprint.py
@@ -1,7 +1,8 @@
 from _datetime import datetime
 import pandas as pd
 import pytest
-from django.test import TestCase
+from django.core.management import call_command
+from django.urls import reverse
 from accounts.models import User
 from user_story.models import UserStory
 from type_us.models import TypeUS
@@ -9,143 +10,168 @@ from projects.models import Project
 from sprints.models import Sprint, SprintMember
 
 
-class TestSprints(TestCase):
+@pytest.fixture
+def create_logged_user(client) -> User:
+    call_command('loaddata', 'permissions')
+    call_command('loaddata', 'permissionsProj')
+    call_command('loaddata', 'default_roles_system')
+    # call_command('loaddata', 'admin')
 
-    fixtures = ['default_roles_system.json', 'permissions.json', 'permissionsProj.json', 'admin.json']
+    data = {
+        'username':  'test',
+    'first_name':  'first_name',
+    'last_name':  'last_name',
+    'email':  'email@gmail.com',
+    'password':  'password',
+    'role_sys':  1,
+    'role_system':  1,
+    }
+    client.post(reverse('accounts.validate_user'),data=data, follow=True)
 
-    @pytest.mark.django_db
-    def test_create_sprint(self):
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
-        assert us_type.name == 'name_test', "Error al crear Tipo de US"
-        user_story = UserStory.objects.create(
-            title='user_story_de_prueba',
-            description='descripcion prueba',
-            business_value=10,
-            technical_priority=20,
-            estimation_time=30,
-            project_id=project.id,
-            us_type_id=us_type.id
-        )
-        sprint = Sprint.objects.create(sprint_name='Sprint Test',
-                                       start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                                       end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
-                                       duration=pd.bdate_range(start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                                                               end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
-                                       number=1,
-                                       project_id=project.id)
+    user = User.objects.get(username="test")
+    client.force_login(user)
+    return user
 
-        self.assertEqual(sprint.sprint_name, 'Sprint Test', "No se ha creado correctamente el sprint")
 
-        user_story.sprint_id = sprint.id
-        user_story.save()
+@pytest.mark.django_db
+def test_create_sprint(create_logged_user):
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
+    assert us_type.name == 'name_test', "Error al crear Tipo de US"
+    user_story = UserStory.objects.create(
+        title='user_story_de_prueba',
+        description='descripcion prueba',
+        business_value=10,
+        technical_priority=20,
+        estimation_time=30,
+        project_id=project.id,
+        us_type_id=us_type.id
+    )
+    sprint = Sprint.objects.create(sprint_name='Sprint Test',
+                                   start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+                                   end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
+                                   duration=pd.bdate_range(start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+                                                           end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
+                                   number=1,
+                                   project_id=project.id)
 
-        self.assertEqual(user_story.sprint.id, sprint.id, "No se han asociado correctamente el sprint y la US")
+    assert sprint.sprint_name == 'Sprint Test', "No se ha creado correctamente el sprint"
 
-    def test_update_sprint(self):
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    user_story.sprint_id = sprint.id
+    user_story.save()
 
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
-        assert us_type.name == 'name_test', "Error al crear Tipo de US"
+    assert user_story.sprint.id == sprint.id, "No se han asociado correctamente el sprint y la US"
 
-        sprint = Sprint.objects.create(
+
+@pytest.mark.django_db
+def test_update_sprint(create_logged_user):
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
+    assert us_type.name == 'name_test', "Error al crear Tipo de US"
+
+    sprint = Sprint.objects.create(
+        sprint_name='Sprint Test',
+        start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+        end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
+        duration=pd.bdate_range(
+            start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+            end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
+        number=1,
+        project_id=project.id
+    )
+    sprint.save()
+
+    sprint.sprint_name = "Sprint Test 2"
+    sprint.description = "Descripcion"
+    sprint.duration = 10
+    sprint.save()
+
+    assert sprint.sprint_name == "Sprint Test 2", "Error al actualizar nombre de sprint"
+    assert sprint.description == "Descripcion", "Error al actualizar Descripcion de sprint"
+    assert sprint.duration == 10, "Error al actualizar duración de sprint"
+
+
+@pytest.mark.django_db
+def test_cancel_sprint(create_logged_user):
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
+
+    sprint = Sprint.objects.create(
+        sprint_name='Sprint Test',
+        start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+        end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
+        duration=pd.bdate_range(
+            start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+            end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
+        number=1,
+        project_id=project.id
+    )
+    sprint.save()
+
+    spr = Sprint.objects.get(id=sprint.id)
+
+    spr.cancellation_reason = "por razones de testing"
+    spr.status = 'Cancelado'
+    spr.save()
+
+    assert spr.status == 'Cancelado', "El sprint no se ha cancelado"
+    assert spr.cancellation_reason == 'por razones de testing', "Error al guardar motivo de cancelacion de sprint"
+
+
+@pytest.mark.django_db
+def test_assign_user_story(create_logged_user):
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
+    assert us_type.name == 'name_test', "Error al crear Tipo de US"
+
+    user_story = UserStory.objects.create(
+        title='user_story_de_prueba',
+        description='descripcion prueba',
+        business_value=10,
+        technical_priority=20,
+        estimation_time=30,
+        project_id=project.id,
+        us_type_id=us_type.id
+    )
+
+    sprint = Sprint.objects.create(
             sprint_name='Sprint Test',
             start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
             end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
-            duration=pd.bdate_range(
-                start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
+            duration=pd.bdate_range(start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
+                                   end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
             number=1,
             project_id=project.id
-        )
-        sprint.save()
+    )
 
-        sprint.sprint_name = "Sprint Test 2"
-        sprint.description = "Descripcion"
-        sprint.duration = 10
-        sprint.save()
+    member = SprintMember.objects.create(sprint_id=sprint.id, user_id=user.id, workload=8)
 
-        assert sprint.sprint_name == "Sprint Test 2", "Error al actualizar nombre de sprint"
-        assert sprint.description == "Descripcion", "Error al actualizar Descripcion de sprint"
-        assert sprint.duration == 10, "Error al actualizar duración de sprint"
+    user_story.assigned_to = member
+    user_story.sprint_id = sprint.id
+    user_story.save()
 
-    def test_cancel_sprint(self):
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    us = UserStory.objects.get(id=user_story.id)
 
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
-
-        sprint = Sprint.objects.create(
-            sprint_name='Sprint Test',
-            start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-            end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
-            duration=pd.bdate_range(
-                start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
-            number=1,
-            project_id=project.id
-        )
-        sprint.save()
-
-        spr = Sprint.objects.get(id=sprint.id)
-
-        spr.cancellation_reason = "por razones de testing"
-        spr.status = 'Cancelado'
-        spr.save()
-
-        assert spr.status == 'Cancelado', "El sprint no se ha cancelado"
-        assert spr.cancellation_reason == 'por razones de testing', "Error al guardar motivo de cancelacion de sprint"
-
-    def test_assign_user_story(self):
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
-        assert us_type.name == 'name_test', "Error al crear Tipo de US"
-
-        user_story = UserStory.objects.create(
-            title='user_story_de_prueba',
-            description='descripcion prueba',
-            business_value=10,
-            technical_priority=20,
-            estimation_time=30,
-            project_id=project.id,
-            us_type_id=us_type.id
-        )
-
-        sprint = Sprint.objects.create(
-                sprint_name='Sprint Test',
-                start_at=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                end_at=datetime.strptime('2022/10/15', '%Y/%m/%d'),
-                duration=pd.bdate_range(start=datetime.strptime('2022/09/30', '%Y/%m/%d'),
-                                       end=datetime.strptime('2022/10/15', '%Y/%m/%d')).size,
-                number=1,
-                project_id=project.id
-        )
-
-        member = SprintMember.objects.create(sprint_id=sprint.id, user_id=user.id, workload=8)
-
-        user_story.assigned_to = member
-        user_story.sprint_id = sprint.id
-        user_story.save()
-
-        us = UserStory.objects.get(id=1)
-
-        assert us.assigned_to.user.username == 'test_user', "No se ha podido asignar el encargado a la historia de usuario"
+    assert us.assigned_to.user.username == 'test_user', "No se ha podido asignar el encargado a la historia de usuario"
--- tests/test_user_stories.py
+++ tests/test_user_stories.py
@@ -1,63 +1,87 @@
 import pytest
+from django.core.management import call_command
 from django.test import TestCase
+from django.urls import reverse
+
 from accounts.models import User
 from user_story.models import UserStory
 from type_us.models import TypeUS
 from projects.models import Project
 
 
-class TestUserStories(TestCase):
-    fixtures = ['default_roles_system.json', 'permissions.json', 'permissionsProj.json', 'admin.json']
+@pytest.fixture
+def create_logged_user(client) -> User:
+    call_command('loaddata', 'permissions')
+    call_command('loaddata', 'permissionsProj')
+    call_command('loaddata', 'default_roles_system')
+    # call_command('loaddata', 'admin')
 
-    @pytest.mark.django_db
-    def test_create_user_story(self):
-        # create user
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
-        assert us_type.name == 'name_test', "Error al crear Tipo de US"
-        user_story = UserStory.objects.create(
-            title='user_story_de_prueba',
-            description='descripcion prueba',
-            business_value=10,
-            technical_priority=20,
-            estimation_time=30,
-            project_id=project.id,
-            us_type_id=us_type.id
-        )
-        assert user_story.project_id == project.id, "Error al asignar id del proyecto a la historia de Usuario"
-        assert user_story.us_type == us_type, "Error al asignar tipo de US a la historia de Usuario"
+    data = {
+        'username':  'test',
+    'first_name':  'first_name',
+    'last_name':  'last_name',
+    'email':  'email@gmail.com',
+    'password':  'password',
+    'role_sys':  1,
+    'role_system':  1,
+    }
+    client.post(reverse('accounts.validate_user'),data=data, follow=True)
 
-    def test_cancel_user_story(self):
-        # create user
-        user = User.objects.create(username='test_user', password='password')
-        user.save()
-        project = Project.objects.create(name='proyecto de juan', description='test_project')
-        assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
-        us_type = TypeUS.objects.create(prefix='test', name='name_test',
-                                        flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
-                                              ('Finalizado', 'Finalizado')], project=project)
+    user = User.objects.get(username="test")
+    client.force_login(user)
+    return user
 
-        user_story = UserStory.objects.create(
-            title='user_story_de_prueba',
-            description='descripcion prueba',
-            business_value=10,
-            technical_priority=20,
-            estimation_time=30,
-            project_id=project.id,
-            us_type_id=us_type.id
-        )
 
-        user_story.cancellation_reason = "por razones de testing"
-        user_story.current_status = 'canceled'
-        user_story.save()
+@pytest.mark.django_db
+def test_create_user_story(create_logged_user):
+    # create user
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
+    assert us_type.name == 'name_test', "Error al crear Tipo de US"
+    user_story = UserStory.objects.create(
+        title='user_story_de_prueba',
+        description='descripcion prueba',
+        business_value=10,
+        technical_priority=20,
+        estimation_time=30,
+        project_id=project.id,
+        us_type_id=us_type.id
+    )
+    assert user_story.project_id == project.id, "Error al asignar id del proyecto a la historia de Usuario"
+    assert user_story.us_type == us_type, "Error al asignar tipo de US a la historia de Usuario"
 
-        us = UserStory.objects.get(id=1)
 
-        assert us.current_status == 'canceled', "La historia de usuario no se ha cancelado"
-        assert us.cancellation_reason == 'por razones de testing', "Error al guardar motivo de cancelacion"
+@pytest.mark.django_db
+def test_cancel_user_story(create_logged_user):
+    # create user
+    user = User.objects.create(username='test_user', password='password')
+    user.save()
+    project = Project.objects.create(name='proyecto de juan', description='test_project')
+    assert project.name == 'proyecto de juan', 'Error al crar el proyecto'
+    us_type = TypeUS.objects.create(prefix='test', name='name_test',
+                                    flow=[('Pendiente', 'Pendiente'), ('Haciendo', 'Haciendo'),
+                                          ('Finalizado', 'Finalizado')], project=project)
 
+    user_story = UserStory.objects.create(
+        title='user_story_de_prueba',
+        description='descripcion prueba',
+        business_value=10,
+        technical_priority=20,
+        estimation_time=30,
+        project_id=project.id,
+        us_type_id=us_type.id
+    )
+
+    user_story.cancellation_reason = "por razones de testing"
+    user_story.current_status = 'canceled'
+    user_story.save()
+
+    us = UserStory.objects.get(id=user_story.id)
+
+    assert us.current_status == 'canceled', "La historia de usuario no se ha cancelado"
+    assert us.cancellation_reason == 'por razones de testing', "Error al guardar motivo de cancelacion"
