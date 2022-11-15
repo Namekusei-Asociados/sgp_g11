91e652548cc2ca5137be2cc4fe4b5ff949e8ce33
rbvaz <romi.alfonzo99@fpuna.edu.py>
2022-11-15 20:52:39 -0300

    resolucion de conflictos requeriments

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
+cd ..
 echo "Iniciando"
-#echo "Clonando repositorio git"
-#rm -Rf sgp_g11
-#git clone https://github.com/Namekusei-Asociados/sgp_g11.git
+echo "Clone de git"
+rm -Rf sgp_g11
+git clone https://github.com/Namekusei-Asociados/sgp_g11.git
 cd sgp_g11
+
 echo "Haciendo checkout del tag $1"
 git checkout $1
+
 if [ $2 = 'produccion' ]
 then
-    echo "Ingresando a Produccion"
-    sh prod.sh $1
+   echo "Ingresando a Produccion"
+   sh deploy/prod.sh $1
 else
-    echo "Ingresando a Desarrollo"
-    sh dev.sh
+   echo "Ingresando a Desarrollo"
+   sh deploy/dev.sh
 fi
-cd ..
-python3 manage.py loaddata user.json
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
-cd ..
+
+
