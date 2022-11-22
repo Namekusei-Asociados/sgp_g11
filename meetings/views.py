78cc90a6426c072477d64cdb2f11c0fb2743083f
notarioale <alealenotario@gmail.com>
2022-11-22 17:47:05 -0300

    added meetings index

--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from projects.models import Project
+
+
+def index(request, id_project):
+
+    project = Project.objects.get(id=id_project)
+    meetings = project.meeting_set.all().order_by('id')
+
+    context = dict(meetings=meetings, id_project=id_project)
+    return render(request, 'meetings/index.html', context)
