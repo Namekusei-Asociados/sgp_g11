16a5bc129bfe56562faf7c90e013785d6c02176c
notarioale <alealenotario@gmail.com>
2022-11-23 16:53:30 -0300

    Documentation

--- meetings/views.py
+++ meetings/views.py
@@ -7,6 +7,14 @@ from meetings.models import Meeting
 
 
 def index(request, id_project):
+    """
+    Muestra la lista de reuniones de un proyecto
+
+    :param request: request
+    :param id_project: id del proyecto al que pertenecen las reuniones
+
+    :return: Documento HTML con la lista de reuniones
+    """
     project = Project.objects.get(id=id_project)
     meetings = project.meeting_set.all().order_by('-meeting_date')
 
@@ -15,15 +23,40 @@ def index(request, id_project):
 
 
 def details(request, id_project, id):
+    """
+    Muestra los detalles de una reunión
+
+    :param request: request
+    :param id_project: id del proyecto al que pertenece la reunión
+    :param id: id de la reunión
+
+    :return: documento HTML con los detalles de la reunión
+    """
     meeting = Meeting.objects.get(id=id)
     return render(request, 'meetings/details.html', {'id_project': id_project, 'meeting': meeting})
 
 
 def create(request, id_project):
+    """
+    Muestra el formulario para crear una reunión
+
+    :param request: request
+    :param id_project: id del proyecto en el que se creará la reunión
+
+    :return: documento HTML con el formulario para crear una reunión
+    """
     return render(request, 'meetings/create.html', {'id_project': id_project})
 
 
 def store(request, id_project):
+    """
+    Crea y guarda una reunión en la base de datos
+
+    :param request: request
+    :param id_project: id del proyecto en el que se creará la reunión
+
+    :return: Documento HTML con la lista de reuniones
+    """
     meeting_name = request.POST['name']
     meeting_date = request.POST['date']
     meeting_details = request.POST['details']
@@ -38,11 +71,29 @@ def store(request, id_project):
 
 
 def edit(request, id_project, id):
+    """
+    Muetsra el formulario para editar una reunión
+
+    :param request: request
+    :param id_project: id del proyecto al que pertenece la reunión
+    :param id: id de la reunión
+
+    :return: Documento HTML con el formulario para editar una reunión
+    """
     meeting = Meeting.objects.get(id=id)
     return render(request, 'meetings/edit.html', {'id_project': id_project, 'meeting': meeting})
 
 
 def update(request, id_project, id):
+    """
+    Actualiza los detalles de una reunión en la base de datos
+
+    :param request: request
+    :param id_project: id del proyecto al que pertenece la reunión
+    :param id: id de la reunión
+
+    :return: Documento HTML con la lista de reuniones
+    """
     meeting_name = request.POST['name']
     meeting_date = request.POST['date']
     meeting_details = request.POST['details']
@@ -56,6 +107,15 @@ def update(request, id_project, id):
 
 
 def destroy(request, id_project, id):
+    """
+    Elimina una reunión de la base de datos
+
+    :param request: request
+    :param id_project: id del proyecto al que pertenece la reunión
+    :param id: id de la reunión
+
+    :return: Documento HTML con la lista de reuniones
+    """
     meeting = Meeting.objects.get(id=id)
     meeting.delete()
     messages.success(request, 'La reunión "' + meeting.meeting_name + '" se ha eliminado correctamente')
